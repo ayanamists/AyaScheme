bbddd1f56f2cbbbee8e7ac21bff234d9a059aea8
ayanamists <ayanamists@gmail.com>
2021-01-27 10:19:03 +0800

    post parser complete

--- ACompilerService/Ast.fs
+++ ACompilerService/Ast.fs
@@ -2,8 +2,22 @@
 
 type Identifier = string
 
+type ExprOp =
+| Add = 0
+| Sub = 1
+| Mult = 2
+| Div = 3
+
+let printOp (op:ExprOp) = 
+    match op with
+    | ExprOp.Add -> "+"
+    | ExprOp.Sub -> "-"
+    | ExprOp.Mult -> "*"
+    | ExprOp.Div -> "/"
+    | _ -> Failure ("Impossible") |> raise
+
 type Expr = 
 | Int of int64
 | Id of Identifier
 | LetExp of ((Identifier * Expr) list) * Expr
-| AppExp of Identifier * (Identifier) list
+| OpExp of ExprOp * Expr * Expr
--- ACompilerService/Parser.fs
+++ ACompilerService/Parser.fs
@@ -28,3 +28,50 @@ let parse str =
     | Success(res, _, _) -> Result.Ok res
     | Failure(errorMsg, _, _) -> Result.Error errorMsg
 
+exception ExcepOfExpToAst of string
+exception NotSExp of string
+
+let rec sExpToAst sexp = 
+    match sexp with
+    | SId id -> Id id 
+    | SInt inner -> Int inner
+    | SExp sl -> 
+        match sl with
+        | [] -> ExcepOfExpToAst ("Impossible") |> raise
+        | hd :: tl -> 
+            match hd with
+            | SId id -> 
+                match id with
+                | "let" -> letToAst tl
+                | "+" -> handleBOp1 ExprOp.Add tl
+                | "-" -> handleBOp2 ExprOp.Sub tl
+                | "*" -> handleBOp1 ExprOp.Mult tl
+                | "/" -> handleBOp2 ExprOp.Div tl
+                | _ -> ExcepOfExpToAst ("Not Implement") |> raise
+            | SInt _ -> (ExcepOfExpToAst "Int should not be called") |> raise
+            | SExp _ -> ExcepOfExpToAst ("Not Implement") |> raise 
+and letToAst lsexp = 
+    match lsexp with
+    | (SExp sl) :: expr :: [] ->
+        ( ( List.map ( fun x ->
+            match x with
+            | SExp (SId x :: y :: []) -> (x, sExpToAst y) 
+            | _ -> ExcepOfExpToAst ("Syntax error") |> raise) sl ), 
+          sExpToAst expr ) |> LetExp
+    | _ -> ExcepOfExpToAst ("Syntax error") |> raise
+and handleBOp1 op lsexp = 
+    match lsexp with
+    | hd1 :: hd2 :: [] -> OpExp (op, sExpToAst hd1, sExpToAst hd2)
+    | hd1 :: hd2 :: tl -> 
+        let Sop = SId (printOp op) in
+        OpExp (op, sExpToAst hd1, SExp (Sop :: hd2 :: tl) |> sExpToAst)
+    | _ -> ExcepOfExpToAst ("Syntax error") |> raise
+and handleBOp2 op lsexp = 
+    match lsexp with
+    | hd1 :: hd2 :: [] -> OpExp (op, sExpToAst hd1, sExpToAst hd2) 
+    | _ -> ExcepOfExpToAst ("Syntax error") |> raise
+
+let parseToAst code = 
+    match parse code with
+    | Result.Ok res -> sExpToAst res
+    | Result.Error err -> NotSExp err |> raise 
